4eeb7e6fb02b77d0d71e3c17730adcec099d18ff
JeevrajEdufund <jeevraj@edufund.in>
2024-03-20 19:46:22 +0530

    dateRange and LandingPage

---
+++ CountDownTimer/src/main/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DateRangePicker from '../sharedComponents/DateRange';
+
+const LandingPage = () => {
+
+
+    return (
+        <>  <DateRangePicker/></>
+    )
+}
+
+export  default LandingPage;
---
+++ CountDownTimer/src/sharedComponents/DateRange.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "./DateRange.css";
+
+const DateRangePicker = () => {
+  //   const [startDate, setStartDate] = useState(null);
+  //   const [endDate, setEndDate] = useState(null);
+  //   const [isOpen, setIsOpen] = useState(false);
+  const [dateValue, setDateValue] = useState(new Date());
+  
+
+  useEffect(() => {
+    console.log(dateValue);
+  }, [dateValue]);
+
+  const changeDateValue = (e) => {
+    setDateValue(e.target.value)
+
+  }
+
+  const handleDateRangeSelected = (start, end) => {
+    setStartDate(start);
+  };
+  return (
+    <>
+      <label for="meeting-time">Choose a time for your appointment:</label>
+
+      <input
+        type="datetime-local"
+        id="meeting-time"
+        name="meeting-time"
+        // value="2018-06-12T19:30"
+        value={dateValue}
+        onChange={changeDateValue}
+        min="2000-06-07T00:00"
+        max="2032-06-14T00:00"
+        
+      />
+      
+    </>
+  );
+};
+
+export default DateRangePicker;
